657a3f3cb0010d9269f41c493a4f784d04eb3c73
LarisaChAng <LChS81@mail.ru>
2022-11-11 12:46:45 +0800

    Добавьте файлы проекта.

---
+++ Progres_ISer/ISeries.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progres_ISer
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currrntValue;
+        public int GetNext()
+        {
+            currrntValue += step;
+            return currrntValue;
+        }
+
+        public void Reset()
+        {
+            currrntValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currrntValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeomProgression : ISeries
+
+    {
+        int step;
+        int startValue;
+        int currrntValue;
+        public int GetNext()
+        {
+            currrntValue *= step;
+            return currrntValue;
+        }
+
+        public void Reset()
+        {
+            currrntValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currrntValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Progres_ISer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progres_ISer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пример работы арифметической прогрессии (начальное значение 10, шаг 2)");
+            ArithProgression arithProgr = new ArithProgression();
+            arithProgr.SetStart(10);
+            arithProgr.SetStep(2);            
+            Console.WriteLine(arithProgr.GetNext());
+            Console.WriteLine(arithProgr.GetNext());
+            Console.WriteLine(arithProgr.GetNext());
+            Console.WriteLine(arithProgr.GetNext());
+            Console.WriteLine("Сброс к начальному значению и повтор сначала:");
+            arithProgr.Reset();
+            Console.WriteLine(arithProgr.GetNext());
+            Console.WriteLine(arithProgr.GetNext());
+
+            Console.WriteLine("Пример работы геометрической прогрессии (начальное значение 10, шаг 2)");
+            GeomProgression geomProgr = new GeomProgression();
+            geomProgr.SetStart(10);
+            geomProgr.SetStep(2);
+            Console.WriteLine(geomProgr.GetNext());
+            Console.WriteLine(geomProgr.GetNext());
+            Console.WriteLine(geomProgr.GetNext());
+            Console.WriteLine("Сброс к начальному значению и повтор сначала:");
+            geomProgr.Reset();
+            Console.WriteLine(geomProgr.GetNext());
+            Console.WriteLine(geomProgr.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
